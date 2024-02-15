fb61e58d2d6909ff8d73739c8bc3de5842b54909
yoji-nakamura <nkmr4ji@gmail.com>
2024-02-15 20:12:37 +0900

    列挙型

--- js/index.js
+++ js/index.js
@@ -43,3 +43,46 @@ console.log(num);
 console.log(typeof (num));
 console.log(str);
 console.log(typeof (str));
+// 列挙型 enum
+var Week;
+(function (Week) {
+    Week[Week["Sunday"] = 0] = "Sunday";
+    Week[Week["Monday"] = 1] = "Monday";
+    Week[Week["Tuesday"] = 2] = "Tuesday";
+    Week[Week["Wednesday"] = 3] = "Wednesday";
+    Week[Week["Thursday"] = 4] = "Thursday";
+    Week[Week["Friday"] = 5] = "Friday";
+    Week[Week["Saturday"] = 6] = "Saturday";
+})(Week || (Week = {}));
+// 連番で０〜
+// const 変数名:列挙型名 = 列挙型名.値
+const sun = Week.Sunday;
+const wed = Week.Wednesday;
+console.log(sun);
+console.log(wed);
+// 数値列挙型
+var WeekNum;
+(function (WeekNum) {
+    WeekNum[WeekNum["Sunday"] = 1] = "Sunday";
+    WeekNum[WeekNum["Monday"] = 2] = "Monday";
+    WeekNum[WeekNum["Tuesday"] = 4] = "Tuesday";
+    WeekNum[WeekNum["Wednesday"] = 8] = "Wednesday";
+    WeekNum[WeekNum["Thursday"] = 16] = "Thursday";
+    WeekNum[WeekNum["Friday"] = 32] = "Friday";
+    WeekNum[WeekNum["Saturday"] = 64] = "Saturday";
+})(WeekNum || (WeekNum = {}));
+const sunnum = WeekNum.Sunday;
+console.log(sunnum);
+// 文字列列挙型
+var WeekString;
+(function (WeekString) {
+    WeekString["Sunday"] = "Sun";
+    WeekString["Monday"] = "Mon";
+    WeekString["Tuesday"] = "Tue";
+    WeekString["Wednesday"] = "Wed";
+    WeekString["Thursday"] = "Thu";
+    WeekString["Friday"] = "Fri";
+    WeekString["Saturday"] = "Sat";
+})(WeekString || (WeekString = {}));
+const sunstr = WeekString.Sunday;
+console.log(sunstr);
